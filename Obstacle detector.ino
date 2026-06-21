0fd16c58f1b0c0ab11c20bd976af8799d92c3ca2
Girish-art <gp5992702@gmail.com>
2026-06-21 16:12:44 +0530

    Update Obstacle detector.ino

--- Obstacle detector.ino
+++ Obstacle detector.ino
@@ -1,7 +1,4 @@
-# Obstacle Detector Using Arduino
 
-## Project Overview
-This project demonstrates an obstacle detection system using Arduino Uno and an ultrasonic sensor. When an obstacle comes within a specified distance, a buzzer is activated to alert the user.
 
 ## Arduino Code
 const int trigPin = 6;
@@ -41,16 +38,3 @@ void loop() {
   delay(100);
 }
 
-## Components Used
-- Arduino Uno
-- HC-SR04 Ultrasonic Sensor
-- Buzzer
-- Breadboard
-- Jumper Wires
-- USB Cable
-
-## Project Video
-<https://youtu.be/UrymY0G8GrU?si=B0PSrwH61UtRX5oV>
-
-## Created By
-Girish Patil
